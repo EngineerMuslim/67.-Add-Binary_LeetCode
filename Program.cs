e217616078a0cc8292fd6f10551092d383d184b7
Abrar Hussein <abrarhussein3434@gmail.com>
2026-08-24 09:13:26 +0300

    Add latest changes

--- Program.cs
+++ Program.cs
@@ -1,1 +1,30 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Text;
+
+class program
+{
+    static string AddBinary(string a, string b)
+    {
+        int i=a.Length - 1, j = b.Length - 1, carry = 0;
+        StringBuilder result = new StringBuilder();
+        while(i >= 0 || j >= 0 || carry > 0)
+        {
+            int sum = carry;
+            if (i >= 0) sum += a[i--] - '0';
+            if (j >= 0) sum += b[j--] - '0';
+            result.Append(sum % 2);
+            carry = sum / 2;
+        }
+        char[] resultArray = result.ToString().ToCharArray();
+        Array.Reverse(resultArray);
+        return new string(resultArray);
+    }
+    static void Main(string[] args)
+    {
+        string a = "1010";
+        string b = "1011";
+
+        string result = AddBinary(a, b);
+
+        Console.WriteLine(result);
+    }
+}
--- obj/Debug/net10.0/67. Add Binary_LeetCode.AssemblyInfo.cs
+++ obj/Debug/net10.0/67. Add Binary_LeetCode.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("67. Add Binary_LeetCode")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b62ecfb940da7aeba2ae7237120d5cf387a9d8fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("67. Add Binary_LeetCode")]
 [assembly: System.Reflection.AssemblyTitleAttribute("67. Add Binary_LeetCode")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
